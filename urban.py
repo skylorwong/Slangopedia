3a2f26cf6e02d53778b612dfe2ff175f94fde47c
Skylor <sw8797@princeton.edu>
2025-04-24 04:00:08 -0400

    no crash

--- urban.py
+++ urban.py
@@ -7,13 +7,13 @@ from bokeh.models import HoverTool
 import numpy as np
 
 app = flask.Flask(__name__, template_folder='.')
-urban_dict_data = data.get_data()
-date_df = data.get_date_df(urban_dict_data)
-date_df_all = data.get_date_df_all(urban_dict_data)
-trends_df = data.get_trends_df(urban_dict_data)
-slang = []
-for key in urban_dict_data:
-  slang.append(key)
+#urban_dict_data = data.get_data()
+#date_df = data.get_date_df(urban_dict_data)
+#date_df_all = data.get_date_df_all(urban_dict_data)
+#trends_df = data.get_trends_df(urban_dict_data)
+#slang = []
+#for key in urban_dict_data:
+  #slang.append(key)
 #pca = data.run_pca(slang)
 pca = None
 
@@ -26,6 +26,10 @@ def index():
 
 @app.route('/language', methods=['GET'])
 def language():
+    urban_dict_data = data.get_data()
+    slang = []
+    for key in urban_dict_data:
+        slang.append(key)
     scripts, divs = vis.get_language_graphs(slang)
     html_code = flask.render_template('language.html', scripts=scripts, divs=divs)
     response = flask.make_response(html_code)
@@ -33,6 +37,12 @@ def language():
 
 @app.route('/explore', methods=['GET'])
 def explore():
+    urban_dict_data = data.get_data()
+    date_df = data.get_date_df(urban_dict_data)
+    date_df_all = data.get_date_df_all(urban_dict_data)
+    slang = []
+    for key in urban_dict_data:
+        slang.append(key)
     scripts, divs, p_graph = vis.get_explore_graphs(urban_dict_data, slang, date_df, date_df_all, pca)
     html_code = flask.render_template('explore.html', scripts=scripts, divs=divs, p_graph=p_graph)
     response = flask.make_response(html_code)
@@ -40,6 +50,7 @@ def explore():
 
 @app.route('/deeper', methods=['GET'])
 def deeper():
+    urban_dict_data = data.get_data()
     scripts, divs = vis.get_deeper_graphs(urban_dict_data)
     html_code = flask.render_template('deeper.html', scripts=scripts, divs=divs)
     response = flask.make_response(html_code)
@@ -47,6 +58,8 @@ def deeper():
 
 @app.route('/trends', methods=['GET'])
 def trends():
+    urban_dict_data = data.get_data()
+    trends_df = data.get_trends_df(urban_dict_data)
     scripts, divs, graphs = vis.get_trends_graphs(trends_df)
     html_code = flask.render_template('trends.html', scripts=scripts, divs=divs, graphs=graphs)
     response = flask.make_response(html_code)
